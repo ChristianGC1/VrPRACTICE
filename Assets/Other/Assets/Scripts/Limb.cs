6e9fc54765ad12461c579fc67d93173b59043c49
ChristianGC1 <1018833@student.sae.edu.au>
2022-08-03 22:46:18 +1000

    Added Zombie spawner, some sounds and Ai system

---
+++ Assets/Other/Assets/Scripts/Limb.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb : MonoBehaviour
+{
+    public bool fatal = false;
+    public GameObject limbPrefab;
+
+    public void Hit()
+    {
+        Limb childlimb = transform.GetChild(0).GetComponentInChildren<Limb>();
+        if (childlimb)
+            childlimb.Hit();
+
+        transform.localScale = Vector3.zero;
+
+        GameObject spawnedLimb = Instantiate(limbPrefab, transform.parent);
+        spawnedLimb.transform.parent = null;
+        Destroy(spawnedLimb, 10);
+
+        if (fatal)
+            GetComponentInParent<Zombie>().Death();
+
+        Destroy(this);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+            Hit();
+    }
+}
